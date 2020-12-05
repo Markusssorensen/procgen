6a410dc39d6aadf85ec06ef7f34ba71c72696966
markus-sorensen <62138902+markus-sorensen@users.noreply.github.com>
2020-12-05 16:07:50 +0100

    m11 og m12

---
+++ jobscript13.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+### General options
+### -- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J model13_hpc
+### -- ask for number of cores (default: 1) --
+#BSUB -n 1
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 23:45
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -u markus.sorensen@live.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o /zhome/69/1/137385/Desktop/DeepLearning/ProjectWork/procgen/logs/%J.out
+#BSUB -e /zhome/69/1/137385/Desktop/DeepLearning/ProjectWork/procgen/logs/%J.err
+# -- end of LSF options --
+
+
+# Load the cuda module
+module load python3/3.8.0
+module load cuda/8.0
+module load cudnn/v7.0-prod-cuda8
+module load ffmpeg/4.2.2
+echo "Running script..."
+cd /zhome/69/1/137385/Desktop/DeepLearning/ProjectWork/procgen/
+ls
+python3 model13_hpc.py
